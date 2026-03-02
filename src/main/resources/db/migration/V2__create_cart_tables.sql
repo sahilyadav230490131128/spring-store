4142e1a76ab07d03b6a9160a2a049e04816c8e32
sahilyadav230490131128 <230490131128.sahil@gmail.com>
2026-03-02 18:58:50 +0530

    flyway repair v2 migration final 2

--- src/main/resources/db/migration/V2__create_cart_tables.sql
+++ src/main/resources/db/migration/V2__create_cart_tables.sql
@@ -1,19 +1,23 @@
-CREATE TABLE cart (
-  id BINARY(16) NOT NULL,
-  date_created DATE NOT NULL DEFAULT (CURRENT_DATE),
-  PRIMARY KEY (id)
-) ENGINE=InnoDB;
+create table carts
+(
+    id           binary(16) default (uuid_to_bin(uuid())) not null
+        primary key,
+    date_created date default (curdate()) not null
+);
 
-CREATE TABLE cart_items (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  cart_id BINARY(16) NOT NULL,
-  product_id BIGINT NOT NULL,
-  quantity INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (id),
-  UNIQUE KEY cart_items_cart_product (cart_id, product_id),
-  KEY cart_items_product_id_fk_idx (product_id),
-  CONSTRAINT cart_items_cart_id_fk
-    FOREIGN KEY (cart_id) REFERENCES cart(id) ON DELETE CASCADE,
-  CONSTRAINT cart_items_product_id_fk
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
-) ENGINE=InnoDB;
+create table cart_items
+(
+    id         bigint auto_increment
+        primary key,
+    cart_id    binary(16)    not null,
+    product_id bigint        not null,
+    quantity   int default 1 not null,
+    constraint cart_items_cart_product_unique
+        unique (cart_id, product_id),
+    constraint cart_items_carts_id_fk
+        foreign key (cart_id) references carts (id)
+            on delete cascade,
+    constraint cart_items_products_id_fk
+        foreign key (product_id) references products (id)
+            on delete cascade
+);
