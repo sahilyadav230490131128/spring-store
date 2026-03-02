a09d1084f018cfecbf4cf46265db7fd6c5a8c02a
sahilyadav230490131128 <230490131128.sahil@gmail.com>
2026-03-02 19:38:23 +0530

    changing V3

--- src/main/resources/db/migration/V3__adding_role_to_user.sql
+++ src/main/resources/db/migration/V3__adding_role_to_user.sql
@@ -1,2 +1,5 @@
-ALTER TABLE `store_api`.`users`
-ADD COLUMN `role` VARCHAR(20) NOT NULL DEFAULT 'USER' AFTER `password`;
+alter table users
+    add role varchar(20) default 'USER' not null;
+
+--ALTER TABLE `store_api`.`users`
+--ADD COLUMN `role` VARCHAR(20) NOT NULL DEFAULT 'USER' AFTER `password`;
